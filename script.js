fe0ca83449c58642d01ef22be4412f318f8c3717
yohannes <yohannesbog@gmail.com>
2020-01-23 19:35:45 -0500

    first js file

---
+++ script.js
@@ -0,0 +1,13 @@
+var two = 2;
+var oneFourth = 1/4;
+
+function marinaraSauce(name) {
+    console.log('Hey, ' + name + ' This is my marinara sauce recipe!" ' + ' Heat a medium-large saucepan over medium heat.');
+    console.log('Hey, ' + name + ' This is my marinara sauce recipe!" ' + ' Add ' + two + ' tspns of oil and 5 carlic cloves, cook until golden, about ' + two + ' minutes');
+    console.log('Hey, ' + name + ' This is my marinara sauce recipe!" ' + ' Add ' + oneFourth + ' cup water, ' + two + ' cans of crushed tomates, and salt and season with black pepper to taste.');
+    console.log('Hey, ' + name + ' This is my marinara sauce recipe!" ' + ' Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes');
+    console.log('Hey, ' + name + ' This is my marinara sauce recipe!" ' + ' Stir in ' + oneFourth + ' cup roughly chopped fresh basil, salt and pepper as needed.');
+     
+}
+
+marinaraSauce('Adam!');
